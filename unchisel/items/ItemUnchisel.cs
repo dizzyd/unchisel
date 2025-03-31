001f75b7dd28740673c15608a84770c9a828bbf6
Dave (Dizzy) Smith <dizzyd@dizzyd.com>
2025-03-31 10:49:47 -0600

    Initial commit

---
+++ unchisel/items/ItemUnchisel.cs
@@ -0,0 +1,79 @@
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
+
+namespace unchisel.items;
+
+internal class ItemUnchisel : Item
+{
+    public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel,
+        bool firstEvent, ref EnumHandHandling handling)
+    {
+        base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
+        if (handling == EnumHandHandling.PreventDefault)
+        {
+            api.Logger.Warning("unchisel.OnHeldInteractStart intercepted (side: {0})", byEntity.World.Side);
+            (api as ICoreClientAPI)?.TriggerIngameError(this, "uhh", "Handled, I guess");
+            return;
+        }
+        
+                
+        // Must be holding a hammer
+        ItemSlot leftSlot = byEntity.LeftHandItemSlot; 
+        if (leftSlot?.Itemstack?.Collectible?.Tool is not EnumTool.Hammer)
+        {
+            (api as ICoreClientAPI)?.TriggerIngameError(this, "nohammer", Lang.Get("Requires a hammer in the off hand"));
+            handling = EnumHandHandling.PreventDefault;
+            return;
+        }
+        
+        // Targeted block must be a microblock
+        // N.B. make sure to use world BlockAccessor, not blockSel.Block as that may not be populated
+        //      on the server side instance of this call
+        Block microBlock = blockSel.Block ?? byEntity.World.BlockAccessor.GetBlock(blockSel.Position);
+        if (microBlock is not BlockMicroBlock) 
+        {
+            api.Logger.Error("OnHeldInteractStart not a microblock side:{0} - pos:{1}", api.World.Side, blockSel.Position);
+            (api as ICoreClientAPI)?.TriggerIngameError(this, "nomicroblock", "Block must be already chiseled");
+            handling = EnumHandHandling.PreventDefault;
+            return;
+        }
+        
+        // Target block must also have a materials list
+        ItemStack microBlockStack = microBlock.OnPickBlock(byEntity.World, blockSel.Position);
+        if (microBlockStack.Attributes["materials"] is not IntArrayAttribute materials)
+        {
+            api.Logger.Error("OnHeldInteractStart no materials array side:{0} - pos:{1}", api.World.Side, blockSel.Position);
+            (api as ICoreClientAPI)?.TriggerIngameError(this, "nomicroblock", "Block must be already chiseled");
+            handling = EnumHandHandling.PreventDefault;
+            return;
+        }
+        
+        if (byEntity.World.Side == EnumAppSide.Server)
+        {
+            // Spawn items that were in the original block
+            foreach (int materialId in materials.value)
+            {
+                Block material = api.World.GetBlock(materialId);
+                api.Logger.Error("OnHeldInteract spawn material: {0}", material);
+                byEntity.World.SpawnItemEntity(new ItemStack(material, 1), blockSel.Position);
+            }
+
+            // Damage the unchisel, based on number of materials that will be returned
+            EntityPlayer player = byEntity as EntityPlayer;
+            if (player != null)
+            {
+                int totalDamage = materials.value.Length;
+                slot.Itemstack.Collectible.DamageItem(byEntity.World, byEntity, player.Player.InventoryManager.ActiveHotbarSlot, totalDamage);
+            }
+            
+            // Delete the old block
+            byEntity.World.BlockAccessor.SetBlock(0, blockSel.Position);
+            byEntity.World.BlockAccessor.MarkBlockModified(blockSel.Position);
+        }
+
+        handling = EnumHandHandling.Handled;
+    }
+}
---
+++ unchisel/unchiselModSystem.cs
@@ -0,0 +1,13 @@
+﻿using unchisel.items;
+using Vintagestory.API.Common;
+
+namespace unchisel;
+
+public class unchiselModSystem : ModSystem
+{
+    public override void Start(ICoreAPI api)
+    {
+        base.Start(api);
+        api.RegisterItemClass(Mod.Info.ModID + ".unchisel", typeof(ItemUnchisel));
+    }
+}
